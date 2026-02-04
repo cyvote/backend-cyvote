86f86843f66511156936c4d943d4cc0abaa4a8d0
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2026-02-04 19:30:18 +0700

    feat: add custom CSS and JS for Swagger UI setup

--- src/main.ts
+++ src/main.ts
@@ -95,6 +95,13 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document, {
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+    ],
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
+    ],
     swaggerOptions: {
       persistAuthorization: true,
     },
